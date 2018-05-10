d37bdeb1ad4810335db80d09a62858ac8363505a
stan wielga <svwielga4@gmail.com>
2018-05-10 12:08:26 -0700

    removed commented out html from index and comments from js

--- public/js/index.js
+++ public/js/index.js
@@ -2,52 +2,51 @@
  * Created by bhavyaagg on 11/01/18.
  */
 
-'use strict';
+"use strict";
 
-$(document).ready(function () {
+$(document).ready(function() {
   const socket = io();
-  const $topBar = $('#topBar');
-  const canvas = document.getElementById('whiteboard');
-  const colors = document.getElementsByClassName('color');
-  const context = canvas.getContext('2d');
+  const $topBar = $("#topBar");
+  const canvas = document.getElementById("whiteboard");
+  const colors = document.getElementsByClassName("color");
+  const context = canvas.getContext("2d");
 
   let current = {
-    color: 'black',
-    emitTo: 'drawing'
+    color: "black",
+    emitTo: "drawing"
   };
   let drawing = false;
 
   $(window).resize(onResize);
   onResize();
 
-  $('#clear').click(function () {
+  $("#clear").click(function() {
     context.clearRect(0, 0, canvas.width, canvas.height);
   });
 
-  canvas.addEventListener('mousedown', onMouseDown);
-  canvas.addEventListener('mouseup', onMouseUp);
-  canvas.addEventListener('mouseout', onMouseUp);
-  canvas.addEventListener('mousemove', throttle(onMouseMove, 5));
+  canvas.addEventListener("mousedown", onMouseDown);
+  canvas.addEventListener("mouseup", onMouseUp);
+  canvas.addEventListener("mouseout", onMouseUp);
+  canvas.addEventListener("mousemove", throttle(onMouseMove, 5));
 
   for (var i = 0; i < colors.length; i++) {
-    colors[i].addEventListener('click', onColorUpdate, false);
+    colors[i].addEventListener("click", onColorUpdate, false);
   }
 
-  socket.on('drawing', onDrawingEvent);
+  socket.on("drawing", onDrawingEvent);
   socket.on("createdSession", onCreatedSession);
   socket.on("joinedSession", onJoinedSession);
   socket.on("drawingInSession", onDrawingInSessionEvent);
 
-  $('#createSession').click(function () {
+  $("#createSession").click(function() {
     socket.emit("createSession");
   });
 
-  $('#joinSession').click(function () {
+  $("#joinSession").click(function() {
     socket.emit("joinSession", {
-      sessionId: $('#sessionIdJoinSession').val()
-    })
+      sessionId: $("#sessionIdJoinSession").val()
+    });
   });
-
 
   function drawLine(x0, y0, x1, y1, color, emit, strokeWidth) {
     y0 -= 50;
@@ -84,7 +83,6 @@ $(document).ready(function () {
       emitToServerObject.sessionId = current.sessionId;
     }
     socket.emit(current.emitTo, emitToServerObject);
-
   }
 
   function onMouseDown(e) {
@@ -111,16 +109,15 @@ $(document).ready(function () {
   }
 
   function onColorUpdate(e) {
-    current.color = e.target.className.split(' ')[1];
+    current.color = e.target.className.split(" ")[1];
   }
 
-  // limit the number of events per second
   function throttle(callback, delay) {
     let previousCall = new Date().getTime();
-    return function () {
+    return function() {
       let time = new Date().getTime();
 
-      if ((time - previousCall) >= delay) {
+      if (time - previousCall >= delay) {
         previousCall = time;
         callback.apply(null, arguments);
       }
@@ -130,22 +127,38 @@ $(document).ready(function () {
   function onDrawingEvent(data) {
     let w = canvas.width;
     let h = canvas.height;
-    drawLine(data.x0 * w, data.y0 * h + 50, data.x1 * w, data.y1 * h + 50, data.color);
+    drawLine(
+      data.x0 * w,
+      data.y0 * h + 50,
+      data.x1 * w,
+      data.y1 * h + 50,
+      data.color
+    );
   }
 
   function onDrawingInSessionEvent(data) {
     let w = canvas.width;
     let h = canvas.height;
-    drawLine(data.x0 * w, data.y0 * h + 50, data.x1 * w, data.y1 * h + 50, data.color, false, data.strokeWidth);
+    drawLine(
+      data.x0 * w,
+      data.y0 * h + 50,
+      data.x1 * w,
+      data.y1 * h + 50,
+      data.color,
+      false,
+      data.strokeWidth
+    );
   }
 
   function onCreatedSession(data) {
     if (data.success) {
-      socket.off('drawing');
+      socket.off("drawing");
       current.sessionId = data.sessionId;
-      current.emitTo = 'drawingInSession';
+      current.emitTo = "drawingInSession";
       $topBar.empty().append(`
-        <div class="col-3 ml-auto text-white">Session Id: ${data.sessionId}</div>    
+        <div class="col-3 ml-auto text-white">Session Id: ${
+          data.sessionId
+        }</div>    
       `);
       context.clearRect(0, 0, canvas.width, canvas.height);
     }
@@ -153,17 +166,18 @@ $(document).ready(function () {
 
   function onJoinedSession(data) {
     if (data.success) {
-      socket.off('drawing');
+      socket.off("drawing");
       current.sessionId = data.sessionId;
-      current.emitTo = 'drawingInSession';
+      current.emitTo = "drawingInSession";
       $topBar.empty().append(`
-        <div class="col-3 ml-auto text-white">Session Id: ${data.sessionId}</div>    
+        <div class="col-3 ml-auto text-white">Session Id: ${
+          data.sessionId
+        }</div>    
       `);
       context.clearRect(0, 0, canvas.width, canvas.height);
     }
   }
 
-  // make the canvas fill its parent
   function onResize() {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
